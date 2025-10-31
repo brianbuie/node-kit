28c21948b117013b904ffd688628ee9bdf0acea6
Brian Buie <brian@buie.dev>
2025-10-31 16:34:04 -0500

    TypeWriter

---
+++ src/TypeWriter.test.ts
@@ -0,0 +1,21 @@
+import { describe, it } from 'node:test';
+import assert from 'node:assert';
+import { TypeWriter } from './TypeWriter.js';
+
+describe('TypeWriter', () => {
+  const test = new TypeWriter('Test');
+
+  it('Creates expected types', async () => {
+    await test.addMember('Member', [
+      {
+        str: 'example string',
+        num: 2,
+        bool: true,
+      },
+    ]);
+    const output = await test.toString();
+    assert(/str:\s+string/.test(output));
+    assert(/num:\s+number/.test(output));
+    assert(/bool:\s+boolean/.test(output));
+  });
+});
---
+++ src/TypeWriter.ts
@@ -0,0 +1,47 @@
+import fs from 'fs';
+import { merge } from 'lodash-es';
+import * as qt from 'quicktype-core';
+
+export class TypeWriter {
+  moduleName;
+  input = qt.jsonInputForTargetLanguage('typescript');
+  outDir;
+  qtSettings;
+
+  constructor(moduleName: string, settings: { outDir?: string } & Partial<qt.Options> = {}) {
+    this.moduleName = moduleName;
+    const { outDir, ...qtSettings } = settings;
+    this.outDir = outDir || './types';
+    const defaultSettings = {
+      lang: 'typescript',
+      rendererOptions: {
+        'just-types': true,
+        'prefer-types': true,
+      },
+      inferEnums: false,
+      inferDateTimes: false,
+    };
+    this.qtSettings = merge(defaultSettings, qtSettings);
+  }
+
+  async addMember(name: string, _samples: any[]) {
+    const samples = _samples.map((s) => (typeof s === 'string' ? s : JSON.stringify(s)));
+    await this.input.addSource({ name, samples });
+  }
+
+  async toString() {
+    const inputData = new qt.InputData();
+    inputData.addInput(this.input);
+    const result = await qt.quicktype({
+      inputData,
+      ...this.qtSettings,
+    });
+    return result.lines.join('\n');
+  }
+
+  async toFile() {
+    const result = await this.toString();
+    fs.mkdirSync(this.outDir, { recursive: true });
+    fs.writeFileSync(`${this.outDir}/${this.moduleName}.d.ts`, result);
+  }
+}
