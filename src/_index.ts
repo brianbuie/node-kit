1eb9af606b361fd0f2fb758945fb528ac39adc37
Brian Buie <brian@buie.dev>
2025-11-02 12:55:16 -0600

    + timeout; move types to devDependencies

--- src/_index.ts
+++ src/_index.ts
@@ -3,4 +3,5 @@ export { Fetcher, type Route, Query, FetchOptions } from './Fetcher.js';
 export { Jwt } from './Jwt.js';
 export { Log } from './Log.js';
 export { snapshot } from './snapshot.js';
+export { timeout } from './timeout.js';
 export { TypeWriter } from './TypeWriter.js';
---
+++ src/timeout.test.ts
@@ -0,0 +1,11 @@
+import { describe, it } from 'node:test';
+import assert from 'node:assert';
+import { timeout } from './timeout.js';
+
+describe('timeout', () => {
+  it('Waits correct amount of time', async () => {
+    const start = Date.now();
+    await timeout(500);
+    assert(Date.now() - start >= 500);
+  });
+});
