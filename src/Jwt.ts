1cf1a44120aba8aee64193da55e8ca130796411e
Brian Buie <brian@buie.dev>
2025-11-02 19:38:53 -0600

    refactor Jwt

--- src/Jwt.ts
+++ src/Jwt.ts
@@ -1,31 +1,22 @@
 import { default as jsonwebtoken, type JwtPayload, type SignOptions } from 'jsonwebtoken';
 import { merge } from 'lodash-es';
 
-export class Jwt {
+type JwtConfig = {
   payload: JwtPayload;
   options: SignOptions;
   seconds: number;
   key: string;
+};
+
+export class Jwt {
+  config;
   #saved?: {
     exp: number;
     token: string;
   };
 
-  constructor({
-    payload,
-    options,
-    seconds,
-    key,
-  }: {
-    payload: JwtPayload;
-    options: SignOptions;
-    seconds: number;
-    key: string;
-  }) {
-    this.payload = payload;
-    this.options = options;
-    this.seconds = seconds;
-    this.key = key;
+  constructor(config: JwtConfig) {
+    this.config = config;
     this.#createToken();
   }
 
@@ -33,24 +24,24 @@ export class Jwt {
     return Math.floor(Date.now() / 1000);
   }
 
-  get token() {
-    if (this.#saved && this.#saved.exp > this.now) {
-      return this.#saved.token;
-    }
-    return this.#createToken();
-  }
-
   #createToken() {
-    const exp = this.now + this.seconds;
+    const exp = this.now + this.config.seconds;
     const payload: JwtPayload = merge(
       {
         iat: this.now,
         exp,
       },
-      this.payload
+      this.config.payload
     );
-    const token = jsonwebtoken.sign(payload, this.key, this.options);
+    const token = jsonwebtoken.sign(payload, this.config.key, this.config.options);
     this.#saved = { token, exp };
     return token;
+  }
+
+  get token() {
+    if (this.#saved && this.#saved.exp > this.now) {
+      return this.#saved.token;
+    }
+    return this.#createToken();
   }
 }
